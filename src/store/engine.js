009bf92d31aa618df208a4d4a29a3a52669b048b
JM <j.maxwell.alan@gmail.com>
2021-09-25 03:17:31 -0400

    capture ticker & adjust logic consequently

--- src/store/engine.js
+++ src/store/engine.js
@@ -1,17 +1,21 @@
-import { Application } from '@pixi/app';
+import { Application } from 'pixi.js';
 
 const SET_CANVAS = 'engine/CANVAS';
-const READY = 'engine/READY';
+const START = 'engine/START';
 const ADD_SPRITE = 'engine/ADD_SPRITE';
+const READY_ENGINE = 'engine/READY';
 const CAPTURE_STAGE = 'engine/CAPTURE_STAGE';
+const CAPTURE_TICKER = 'engine/CAPTURE_TICKER';
+const READY_PIVOT = 'engine/SPRITES/READY_PIVOT';
+const TRASH_SPRITE = 'engine/SPRITES/TRASH';
 
 export const SetCanvas = canvas => ({
   type: SET_CANVAS,
   canvas
 });
 
-export const ReadyEngine = () => ({
-  type: READY
+export const StartEngine = () => ({
+  type: START
 });
 
 export const AddSprite = (key, sprite) => ({
@@ -24,12 +28,33 @@ export const CaptureStage = () => ({
   type: CAPTURE_STAGE
 });
 
+export const CaptureTicker = () => ({
+  type: CAPTURE_TICKER
+});
+
+export const ReadyEngine = () => ({
+  type: READY_ENGINE
+});
+
+export const ReadyPivot = key => ({
+  type: READY_PIVOT,
+  key
+});
+
+export const TrashSprite = key => ({
+  type: TRASH_SPRITE,
+  key
+});
+
 export default function reducer (
   state = {
     core: null,
     canvas: null,
+    ticker: null,
     stage: null,
     ready: false,
+    stageReady: false,
+    tickerReady: false,
     sprites: {}
   },
   { type, canvas, key, sprite }
@@ -40,20 +65,31 @@ export default function reducer (
         ...state,
         canvas
       };
-    case READY:
+    case START:
       return {
         ...state,
         core: new Application({
           view: state.canvas,
           height: window.innerHeight,
           width: window.innerWidth
-        }),
-        ready: true
+        })
       };
     case CAPTURE_STAGE:
       return {
         ...state,
-        stage: state.core.stage
+        stage: state.core.stage,
+        stageReady: true
+      };
+    case CAPTURE_TICKER:
+      return {
+        ...state,
+        ticker: state.core.ticker,
+        tickerReady: true
+      };
+    case READY_ENGINE:
+      return {
+        ...state,
+        ready: true
       };
     case ADD_SPRITE:
       state.stage.addChild(sprite);
@@ -61,7 +97,22 @@ export default function reducer (
         ...state,
         sprites: {
           ...state.sprites,
-          [key]: sprite
+          [key]: {
+            object: sprite,
+            pivotReady: false,
+            trashable: false
+          }
+        }
+      };
+    case READY_PIVOT:
+      return {
+        ...state,
+        sprites: {
+          ...state.sprites,
+          [key]: {
+            ...state.sprites[key],
+            pivotReady: true
+          }
         }
       };
     default:
